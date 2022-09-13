9ecabce0b0ac805dd1bfa25960d14dbb9e057979
Gabriel <grossiwm@gmail.com>
2022-09-13 00:40:46 -0300

    Enable DD metrics and refactor dockerization job

--- src/main/java/com/newsprovider/portal/PortalApplication.java
+++ src/main/java/com/newsprovider/portal/PortalApplication.java
@@ -2,8 +2,10 @@ package com.newsprovider.portal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.metrics.export.datadog.EnableDatadogMetrics;
 
 @SpringBootApplication
+@EnableDatadogMetrics
 public class PortalApplication {
 
 	public static void main(String[] args) {
