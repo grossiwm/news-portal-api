3d1bdd89158d0a1d17dfef4539634e4692d1feae
Gabriel <grossiwm@gmail.com>
2022-09-13 00:59:44 -0300

    Fix metrics

--- src/main/java/com/newsprovider/portal/PortalApplication.java
+++ src/main/java/com/newsprovider/portal/PortalApplication.java
@@ -2,10 +2,8 @@ package com.newsprovider.portal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.metrics.export.datadog.EnableDatadogMetrics;
 
 @SpringBootApplication
-@EnableDatadogMetrics
 public class PortalApplication {
 
 	public static void main(String[] args) {
